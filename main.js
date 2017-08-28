e485e53da308709ce7b43324d2f77777a28493a6
Eliud Uresti <urestieliud@gmail.com>
2017-08-28 17:40:04 -0600

    added basic geolocation code; need to fix html

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
 $(document).ready(function(){
 
+    var getLo = document.getElementById("demo");
+
+    function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        getLo.innerHTML = "Geolocation is not supported by this browser.";
+    }
+};
+
+function showPosition(position) {
+    getLo.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude; 
+}
+
 });
